47b131aba967f56ac9b3510b56c7987417de2709
BrianMwengi <mwengibrian@.gmailcom>
2021-07-07 14:34:58 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+   './index.html', //remove unused css
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
